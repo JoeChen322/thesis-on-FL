0a8353133e08a327740375c6f21f25626c112dec
JoeChen322 <1138932518@qq.com>
2026-08-16 21:18:33 +0200

    edit model to resnet-18

--- split_learning_utils.py
+++ split_learning_utils.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset, default_collate
 from torchvision import datasets, transforms
 
@@ -41,37 +40,112 @@ def configure_torch_threads(num_threads):
     return torch.get_num_threads()
 
 
-class ClientNet(nn.Module):
-    def __init__(self):
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.fc1 = nn.Linear(28 * 28, 128)
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels * self.expansion:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels * self.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(out_channels * self.expansion),
+            )
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
-        return F.relu(self.fc1(x))
+        identity = x
+
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        return self.relu(out)
 
 
-class CifarClientNet(nn.Module):
-    def __init__(self):
+def make_resnet_layer(in_channels, out_channels, num_blocks, stride=1):
+    layers = [BasicBlock(in_channels, out_channels, stride)]
+    for _ in range(1, num_blocks):
+        layers.append(BasicBlock(out_channels, out_channels))
+    return nn.Sequential(*layers)
+
+
+class ResNet18ClientNet(nn.Module):
+    def __init__(self, input_channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.conv1 = nn.Conv2d(
+            input_channels,
+            64,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = make_resnet_layer(64, 64, num_blocks=2)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(x.size(0), -1)
-        return F.relu(self.fc1(x))
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+        return self.layer1(x)
+
+
+class ClientNet(ResNet18ClientNet):
+    def __init__(self):
+        super().__init__(input_channels=1)
+
+
+class CifarClientNet(ResNet18ClientNet):
+    def __init__(self):
+        super().__init__(input_channels=3)
 
 
 class ServerNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc2 = nn.Linear(128, 10)
+        self.layer2 = make_resnet_layer(64, 128, num_blocks=2, stride=2)
+        self.layer3 = make_resnet_layer(128, 256, num_blocks=2, stride=2)
+        self.layer4 = make_resnet_layer(256, 512, num_blocks=2, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, 10)
 
     def forward(self, smashed_data):
-        return self.fc2(smashed_data)
+        x = self.layer2(smashed_data)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        return self.fc(x)
 
 
 class FullNet(nn.Module):
